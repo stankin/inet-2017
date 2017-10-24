7bbc269c69c229718c991a64dfa941e9119d03a7
pindos11 <azudilow@gmail.com>
2017-10-24 11:58:34 +0300

    Update scr.js

--- idm-17-01/scr.js
+++ idm-17-01/scr.js
@@ -5,7 +5,9 @@ req.onreadystatechange = function(){
 	udata = req.responseText;
 }
 req.send();
+alert(udata);
 var ulist = udata.split("/n");
 for (var i = 0; i < ulist.length; i++){
 	console.log(ulist[i]);
+	alert(ulist[i]);
 }
