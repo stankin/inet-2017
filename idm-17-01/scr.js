8c3e59827e4fc280fdea495a6d7a22d45eaf8be7
pindos11 <azudilow@gmail.com>
2017-10-24 14:38:17 +0300

    change

--- idm-17-01/scr.js
+++ idm-17-01/scr.js
@@ -5,15 +5,26 @@ req.onreadystatechange = function(){
 	if(req.readyState != 4) return;
 	if(req.status == 200){
 		udata = req.responseText;
-		alert("Ok!");
 	}
 	else{
-		alert("KEK");
 		return;
 	}
 	var ulist = udata.split("/n");
 	for (var i = 0; i < ulist.length; i++){
-		console.log(ulist[i]);
+		var sudata = ulist[i].split("!!!");
+		if(sudata.length < 6 || sudata[0]=="#"){
+			continue;
+		}
+		var udiv = document.createElement('div');
+		udiv.className = 'idm_user';
+		
+		udiv.innerHTML += "<br>"+sudata[0];
+		udiv.innerHTML += " "+sudata[1];
+		udiv.innerHTML += " "+sudata[2];
+		udiv.innerHTML += " <a href="+sudata[3]+">Page link</a>";
+		udiv.innerHTML += " "+sudata[4];
+		udiv.innerHTML += " "+sudata[5];
+		udiv.innerHTML += "<hr>"
 	}
 }
 req.send();
