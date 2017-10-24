f6f2b6ad979eaf2b1e85c76986c12a56f38e5673
pindos11 <azudilow@gmail.com>
2017-10-24 12:36:57 +0300

    Add files via upload

--- idm-17-01/scr.js
+++ idm-17-01/scr.js
@@ -1,12 +1,19 @@
 var udata = "";
 var req = new XMLHttpRequest();
-req.open('GET','data.txt');
-req.onload = function(){
-	udata = req.responseText;
+req.open('GET','data.txt',true);
+req.onreadystatechange = function(){
+	if(req.readyState != 4) return;
+	if(req.status == 200){
+		udata = req.responseText;
+		alert("Ok!");
+	}
+	else{
+		alert("KEK");
+		return;
+	}
+	var ulist = udata.split("/n");
+	for (var i = 0; i < ulist.length; i++){
+		console.log(ulist[i]);
+	}
 }
 req.send();
-alert(udata);
-var ulist = udata.split("/n");
-for (var i = 0; i < ulist.length; i++){
-	console.log(ulist[i]);
-}
