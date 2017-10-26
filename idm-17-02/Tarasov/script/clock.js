eaabb84c0a558d322fbb0b0aa8127774d5e6594b
ARDev1161 <radtarasov@gmail.com>
2017-10-26 19:23:26 +0300

    Create clock.js

---
+++ idm-17-02/Tarasov/script/clock.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+  function digitalWatch() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    document.getElementById("digital_watch").innerHTML = hours + ":" + minutes + ":" + seconds;
+    setTimeout("digitalWatch()", 1000);
+  }
+</script>
