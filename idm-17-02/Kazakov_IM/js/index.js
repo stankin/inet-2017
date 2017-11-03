ab15cc8e816f5cb8a5b7c234cad5bde220d8346e
KazakovIvan <32494125+KazakovIvan@users.noreply.github.com>
2017-11-04 01:16:42 +0300

    Update index.js

--- idm-17-02/Kazakov_IM/js/index.js
+++ idm-17-02/Kazakov_IM/js/index.js
@@ -1,1 +1,33 @@
+$(function() {
+
+      setInterval( function() {
+      var seconds = new Date().getSeconds();
+      var sdegree = seconds * 6;
+      var srotate = "rotate(" + sdegree + "deg)";
+
+      $("#sec").css({ "transform": srotate });
+
+      }, 1000 );
+
+      setInterval( function() {
+      var hours = new Date().getHours();
+      var mins = new Date().getMinutes();
+      var hdegree = hours * 30 + (mins / 2);
+      var hrotate = "rotate(" + hdegree + "deg)";
+
+      $("#hour").css({ "transform": hrotate});
+
+      }, 1000 );
+
+      setInterval( function() {
+      var mins = new Date().getMinutes();
+      var mdegree = mins * 6;
+      var mrotate = "rotate(" + mdegree + "deg)";
+
+      $("#min").css({ "transform" : mrotate });
+
+      }, 1000 );
+
+});
+
 
