5bd808ae5568bda1297a7c00b4697dff3209da9b
IvanAstakhov <32309410+IvanAstakhov@users.noreply.github.com>
2017-11-18 01:56:29 +0300

    Update index.js

--- idm-17-02/Astahov_IA/js/index.js
+++ idm-17-02/Astahov_IA/js/index.js
@@ -1,1 +1,11 @@
+function show(Element){
+    var display =document.getElementById(Element).style.display;
+
+    if(display==="block"){
+        document.getElementById(Element).style.display='none';
+    }else{
+        document.getElementById(Element).style.display='block';
+    }
+
+};
 
