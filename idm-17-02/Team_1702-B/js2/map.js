3e7b650b72655139c2490ff8424630eb8b638627
ARDev1161 <radtarasov@gmail.com>
2017-12-21 02:00:16 +0300

    Update map.js

--- idm-17-02/Team_1702-B/js2/map.js
+++ idm-17-02/Team_1702-B/js2/map.js
@@ -1,4 +1,8 @@
 function initMap() {
+         var stankin = {lat: 55.789745, lng: 37.595052};
+         var map = new google.maps.Map(document.getElementById('map'),{zoom: 4,center: stankin});
+         var marker = new google.maps.Marker({position: stankin,map: map});
+	 var KmlLayer = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/Russia.kml',map: map});
   
 /*  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
