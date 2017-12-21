48f4659d820940523b6b966526b96967ad851509
ARDev1161 <radtarasov@gmail.com>
2017-12-22 01:39:00 +0300

    Update map.js

--- idm-17-02/Team_1702-B/js2/map.js
+++ idm-17-02/Team_1702-B/js2/map.js
@@ -2,7 +2,12 @@ function initMap() {
          var stankin = {lat: 55.789745, lng: 37.595052};
          var map = new google.maps.Map(document.getElementById('map'),{zoom: 4,center: stankin});
          var marker = new google.maps.Marker({position: stankin,map: map});
-	 var KmlLayer = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/africa.kml',map: map});
+	 var AfricaKml = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/africa.kml',map: map});
+  	 var AsiaKmlr = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/asia.kml',map: map});
+   	 var AustraliaKml = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/australia.kml',map: map});
+ 	 var EuropeKml = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/europe.kml',map: map});
+ 	 var NorthAmericaKml = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/north_america.kml',map: map});
+ 	 var SouthAmericaKml = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/south_america.kml',map: map});
   
 /*  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
