f5555572270252ba4629d9f77f49e8837e566dd8
KalininVO <34547560+KalininVO@users.noreply.github.com>
2017-12-29 21:09:45 +0300

    Add files via upload

---
+++ idm-17-01/Kalinin/Shop/res/x5settings.js
@@ -0,0 +1,115 @@
+(function ( $, x5engine ) {
+	var x = x5engine,
+		s = x.settings,
+		p = s.currentPath,
+		b = x.boot;
+
+	s.siteId = 'DEB3CD61E7571786259F28444BE56C79';
+	s.version = '12-0-9-30';
+	b.push(function () {
+		x.setupDateTime();
+		x.imAccess.showLogout();
+		x.utils.autoHeight();
+		x.cart.ui.updateWidget();
+		x.imGrid.init();
+	});
+	s.menu = {
+		verticalScroll: false,
+		orientation: 'horizontal'
+	};
+	b.push(function () {
+		x.menu({
+			target: '#imMnMn',
+			showCurrent: false,
+			desktopVerticalScroll: false,
+			mobileVerticalScroll: false,
+			showLogoOnScroll: false,
+			orientation: 'horizontal',
+			menuHeight: 37,
+			menuWidth: 120,
+			submenuHeight: 22,
+			submenuWidth: 160,
+			opacity: 0.8,
+			type: 'singleColumn',
+			alignment: 'left',
+			effect: 'fade'
+		});
+	});
+	b.push(function () { x.utils.imPreloadImages([p + 'menu/hor_main.png',p + 'menu/hor_main_h.png',p + 'menu/hor_main_c.png', p + 'res/imLoad.gif', p + 'res/imClose.png']); });
+
+	// ShowBox
+	$.extend(s.imShowBox, {
+		'effect' : 'fade',
+		'shadow' : '',
+		'background' : '#000000',
+		'borderWidth' : {
+			'top': 1,
+			'right': 1,
+			'bottom': 1,
+			'left': 1
+		},
+		buttonRight: {
+			url: p + 'res/b01_r.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		buttonLeft: {
+			url: p + 'res/b01_l.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		'borderRadius' : '5px 5px 5px 5px',
+		'borderColor' : '#000000 #000000 #000000 #000000',
+		'textColor' : '#000000',
+		'fontFamily' : 'Tahoma',
+		'fontStyle' : 'normal',
+		'fontWeight' : 'normal',
+		'fontSize' : '9pt',
+		'textAlignment' : 'left',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7,
+		'radialBg' : true // Works only in Mozilla Firefox and Google Chrome
+	});
+
+	// PopUp
+	$.extend(s.imPopUp, {
+		'effect' : 'fade',
+		'width' : 500,
+		'shadow' : '',
+		'background' : '#000000',
+		'borderRadius' : 10,
+		'textColor' : '#000000',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7
+	});
+
+	// Tip
+	$.extend(s.imTip, {
+		'borderRadius' : 1,
+		'arrow' : true,
+		'position' : 'bottom',
+		'effect' : 'none',
+		'showTail' : true
+	});
+
+	// Captcha
+	b.push(function () {
+		x5engine.captcha.instance = new x5engine.captcha.x5captcha({
+			"phpfile": "captcha/x5captcha.php",
+		});
+	}, false, 'first');
+
+	// BreakPoints
+	s.breakPoints.push({ "hash": "3066a51bdcb5d04c28dac30ce6993a7f", "name": "Desktop", "start": "max", "end": 1048, "fluid": false});
+	s.breakPoints.push({ "hash": "dec599558301699dfc271392a082819c", "name": "Breakpoint 1", "start": 1047, "end": 720, "fluid": false});
+	s.breakPoints.push({ "hash": "b5a6e70dcd45b271d4dbff444ddeed77", "name": "Mobile", "start": 719, "end": 480, "fluid": false});
+	s.breakPoints.push({ "hash": "e81e7c6a4cc72fa678403ac3f73710af", "name": "Mobile Fluid", "start": 479, "end": 0, "fluid": true});
+
+	b.push(function () { x.cookielaw.activateScripts() });
+
+	s.loaded = true;
+})( _jq, x5engine );
---
+++ idm-17-01/Kalinin/Shop/res/x5settings.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'url' => 'http://index.ru/',
+	'homepage_url' => 'http://index.ru/index.html',
+	'public_folder' => '',
+	'salt' => 'b66fehqflcx6tf296bup0k521s71wf193',
+	'use_common_email_sender_address' => false,
+	'common_email_sender_addres' => ''
+);
+
+
+/*
+|--------------------------------------------------------------------------------------
+|	DATABASES SETTINGS
+|--------------------------------------------------------------------------------------
+*/
+
+$imSettings['databases'] = array();
+
+/*
+|-------------------------------------------------------------------------------------------
+|	GUESTBOOK SETTINGS
+|-------------------------------------------------------------------------------------------
+*/
+
+$imSettings['guestbooks'] = array();
+/*
+|-------------------------------------------------------------------------------------------
+|	Dynamic Objects SETTINGS
+|-------------------------------------------------------------------------------------------
+*/
+
+$imSettings['dynamicobjects'] = array(
+);
+
+/*
+|-------------------------------
+|	EMAIL SETTINGS
+|-------------------------------
+*/
+
+$ImMailer->emailType = 'phpmailer';
+$ImMailer->exposeWsx5 = true;
+$ImMailer->header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' . "\n" . '<html>' . "\n" . '<head>' . "\n" . '<meta http-equiv="content-type" content="text/html; charset=utf-8">' . "\n" . '<meta name="generator" content="Incomedia WebSite X5 v12 - www.websitex5.com">' . "\n" . '</head>' . "\n" . '<body bgcolor="#708090" style="background-color: #708090;">' . "\n\t" . '<table border="0" cellpadding="0" align="center" cellspacing="0" style="padding: 0; margin: 0 auto; width: 700px;">' . "\n\t" . '<tr><td id="imEmailContent" style="min-height: 300px; padding: 10px; font: normal normal normal 9pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-align: left; text-decoration: none;  width: 700px;border-style: solid; border-color: #808080; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 0; border-bottom: none; border-left-width: 1px;background-color: #FFFFFF" width="700px">' . "\n\t\t";
+$ImMailer->footer = "\n\t" . '</td></tr>' . "\n\t" . '<tr><td id="imEmailFooter" style="font: normal normal normal 7pt \'Tahoma\'; color: #000000; background-color: transparent; text-align: center; text-decoration: none;  width: 700px;border-style: solid; border-color: #808080; border-top-width: 0; border-top: none; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; padding: 10px; background-color: #FFFFFF" width="700px">' . "\n\t\t" . 'Это письмо-подтверждение предназначено для исключения несанкционированного использования вашего адреса электронной почты.<br>Если вы получили это сообщение по ошибке, пожалуйста, сообщите об этом отправителю и удалите это письмо, не делая копий.' . "\n\t" . '</td></tr>' . "\n\t" . '</table>' . "\n" . '</body>' . "\n" . '</html>';
+$ImMailer->bodyBackground = '#FFFFFF';
+$ImMailer->bodyBackgroundEven = '#FFFFFF';
+$ImMailer->bodyBackgroundOdd = '#F0F0F0';
+$ImMailer->bodyBackgroundBorder = '#CDCDCD';
+$ImMailer->bodySeparatorBorderColor = '#000000';
+$ImMailer->emailBackground = '#708090';
+$ImMailer->emailContentStyle = 'font: normal normal normal 9pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-align: left; text-decoration: none; ';
+$ImMailer->emailContentFontFamily = 'font-family: Tahoma;';
+ImTopic::$captcha_code = "		<div class=\"x5captcha-wrap\">
+			<label>Проверочное слово:</label><br />
+			<input type=\"text\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
+		</div>
+";
+
+// End of file x5settings.php
