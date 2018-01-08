28bd9fc6050153650c87608f2fa05bd9abc01782
Kirill2404 <33125659+Kirill2404@users.noreply.github.com>
2018-01-09 02:54:06 +0300

    Update map.js

--- idm-17-02/Team_1702-B/js2/map.js
+++ idm-17-02/Team_1702-B/js2/map.js
@@ -11,28 +11,22 @@ function initMap() {
  	 var SouthAmericaKml = new google.maps.KmlLayer({url: 'https://sites.google.com/site/radtarasov/kml/south_america.kml',map: map});
 	
   AfricaKml.addListener('click', function(kmlEvent){
-    var text = kmlEvent.featureData.description;
-    showInContentWindow("АФРИКА\n\nТерритория:\n 30 221 532 км²\n\nВключает:\n55 государств\n\nПлотность:\n30,51 чел./км²");
+    infowindow.setContent("АФРИКА\n\nТерритория:\n 30 221 532 км²\n\nВключает:\n55 государств\n\nПлотность:\n30,51 чел./км²");
   });
   AsiaKml.addListener('click', function(kmlEvent) {
-    var text = kmlEvent.featureData.description;
-    showInContentWindow("АЗИЯ\n\nТерритория:\n44 579 000 км²\n\nВключает:\n49 государств\n\nПлотность:\n87 чел./км²");
+    infowindow.setContent("АЗИЯ\n\nТерритория:\n44 579 000 км²\n\nВключает:\n49 государств\n\nПлотность:\n87 чел./км²");
   });
   AustraliaKml.addListener('click', function(kmlEvent) {
-    var text = kmlEvent.featureData.description;
-    showInContentWindow("АВСТРАЛИЯ\n\nТерритория:\n7 692 024 км²\n\nПлотность:\n2,8 чел./км²");
+    infowindow.setContent("АВСТРАЛИЯ\n\nТерритория:\n7 692 024 км²\n\nПлотность:\n2,8 чел./км²");
   });
   EuropeKml.addListener('click', function(kmlEvent) {
-    var text = kmlEvent.featureData.description;
-    showInContentWindow("ЕВРОПА\n\nТерритория:\n10,18 млн км²\n\nВключает:\n50 государств\n\nПлотность:\n72,5 чел./км²");
+    infowindow.setContent("ЕВРОПА\n\nТерритория:\n10,18 млн км²\n\nВключает:\n50 государств\n\nПлотность:\n72,5 чел./км²");
   });
   NorthAmericaKml.addListener('click', function(kmlEvent) {
-    var text = kmlEvent.featureData.description;
-    showInContentWindow("СЕВЕРНАЯ АМЕРИКА\n\nТерритория:\n24,25 млн км²\n\nВключает:\n23 государств\n\nПлотность:\n22,9 чел./км²");
+    infowindow.setContent("СЕВЕРНАЯ АМЕРИКА\n\nТерритория:\n24,25 млн км²\n\nВключает:\n23 государств\n\nПлотность:\n22,9 чел./км²");
   });
   SouthAmericaKml.addListener('click', function(kmlEvent) {
-    var text = kmlEvent.featureData.description;
-    showInContentWindow("ЮЖНАЯ АМЕРИКА\n\nТерритория:\n17,84 млн км²\n\nВключает:\n12 государств\n\nПлотность:\n21,4 чел./км²");
+    infowindow.setContent("ЮЖНАЯ АМЕРИКА\n\nТерритория:\n17,84 млн км²\n\nВключает:\n12 государств\n\nПлотность:\n21,4 чел./км²");
   });
   function showInContentWindow(text) {
     var sidediv = document.getElementById('content-window');
