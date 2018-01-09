36f52addf8c28a80268e2be16761763e4d4fd723
IGOR <34909242+fizigor@users.noreply.github.com>
2018-01-09 12:15:00 +0000

    Update and rename idm-17-04/Fits/project/controller.js to idm-17-04/Fits/project/js/controller.js

---
+++ idm-17-04/Fits/project/js/controller.js
@@ -0,0 +1,13 @@
+document.body.onkeydown = function (e) {
+ var keys = { 
+  37: 'left',
+  39: 'right', 
+  40: 'down',
+  32: 'down', 
+  38: 'rotate', 
+  27: 'escape' 
+ };
+ if (typeof(keys[e.keyCode])!='undefined') { 
+  keyPress (keys[e.keyCode]); 
+  render(); 
+};
